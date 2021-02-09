7e9274909fbd4947f713f0f0b5a3901d8d65bdfa
Deustavo <gustavo.andrade00@outlook.com>
2021-02-09 14:58:01 -0300

    página do formulário inicial

--- Interface/pages/index.js
+++ Interface/pages/index.js
@@ -1,65 +1,51 @@
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const cities = [{name: 'Marília - SP'}, {name: 'Pompéia - SP'}, {name: 'Oriente - SP'}];
+  const cultures = [{name: 'Milho'}, {name: 'Soja'}];
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <form className={styles.container}>
+      <p className={styles.title}>Capro<span>Agro</span></p>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className='inputContainer'>
+        <p className='inputTitle'>Selecione sua cidade</p>
+        <select className='inputContent'>
+          {cities.map(city => {
+            return <option key={city.name} value={city.name}>{city.name}</option>
+          })}
+        </select>
+      </div>
+      
+      <div className='inputContainer'>
+        <p className='inputTitle'>Selecione sua cultura</p>
+        <select className='inputContent'>
+          {cultures.map(culture => {
+            return <option key={culture.name} value={culture.name}>{culture.name}</option>
+          })}
+        </select>
+      </div>
+      
+      
+      <div className='inputDayContainer'>
+        <div className='inputContainer' style={{ marginRight: 8 }}>
+          <p className='inputTitle'>Do dia</p>
+          <input className='inputContent'/>
         </div>
-      </main>
+        <div className='inputContainer'>
+          <p className='inputTitle'>Até o dia</p>
+          <input className='inputContent'/>
+        </div>
+      </div>
+      
+      <div className='inputContainer'>
+        <p className='inputTitle'>Área plantada em hectares</p>
+        <input className='inputContent' type="number"/>
+      </div>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+      <div className='inputContainer' >
+        <input className='buttonSubmit' type="submit" />
+      </div>
+    </form>
   )
 }
