99a32a304ae8111a47fc662166ccdd2773438c93
Deustavo <gustavo.andrade00@outlook.com>
2021-02-26 14:50:18 -0300

    removendo warn

--- app/pages/index.js
+++ app/pages/index.js
@@ -37,7 +37,7 @@ export default function Home() {
     <div>
       <Head>
         <title>CaproAgro</title>
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous" />
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossOrigin="anonymous" />
         <link rel="sortcut icon" href="tt.png" type="image/gif" />
       </Head>
       {homePage == 'loading' ?
